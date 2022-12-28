d42d719a7913b4c0b5d35f854d8f99f1c9f0a77a
Daniel Neves <danielnevesdecastro@gmail.com>
2022-12-28 13:09:00 -0300

    Tipos e variáveis

---
+++ java-anatomia-classes/src/TiposVariaveis.java
@@ -0,0 +1,16 @@
+import java.net.Socket;
+
+import javax.sql.rowset.spi.SyncResolver;
+
+public class TiposVariaveis {
+  public static void main(String[] args) {
+
+    final double VALOR_DE_PI= 3.14;
+    VALOR_DE_PI = 3.15;
+
+    System.out.println(VALOR_DE_PI);
+  }  
+
+    
+
+}
